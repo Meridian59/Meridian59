d1186f9b378e9bfeaea5428a336735da6c2d8662
Tarandeep Shergill <tsShergill91@gmail.com>
2012-11-01 14:03:45 -0700

    [FIX] Beta Potion 2.0
    - No longer requires the word "level"
    - Work with numbers as well.

--- clientd3d/config.c
+++ clientd3d/config.c
@@ -119,7 +119,7 @@ static int   DefaultRedialDelay   = 60;
 static char  DefaultHostname[]    = "cheater";
 static char  DefaultDomainFormat[] = "meridian%dserver.dyndns.org"; // MUST have a %d in it somewhere.
 static char  DefaultSockPortFormat[] = "59%.2d";
-static int   DefaultServerNum     = -1;
+static int   DefaultServerNum     = 59;
 static int   DefaultTimeout       = 20;
 
 /************************************************************************/

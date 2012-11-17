86a4b9258a5b539aeeb2985f574e15d70d79eeaa
cyberjunk <clint.banzhaf@gmx.net>
2012-11-17 05:49:07 +0100

    Update clientd3d/ownerdrw.c
    Show colors of PKs and outlaws in the "Who is online list"

--- clientd3d/ownerdrw.c
+++ clientd3d/ownerdrw.c
@@ -422,15 +422,7 @@ void DrawOwnerListItem(const DRAWITEMSTRUCT *lpdis, Bool selected, Bool combo)
    SelectObject(lpdis->hDC, GetFont(FONT_LIST));
 
 	if (style & OD_COLORTEXT)
-	{
-		// if it's not a priveleged player, don't do the color effects
-		if ((PF_DM != GetPlayerFlags(obj->flags) &&
-		     PF_SUPER != GetPlayerFlags(obj->flags) &&
-		     PF_EVENTCHAR != GetPlayerFlags(obj->flags) &&
-		     PF_CREATOR != GetPlayerFlags(obj->flags)))
-		{
-			style &= ~OD_COLORTEXT;
-		}
+	{		
 
 		// get the color we'd prefer for this particular obj
 		if (style & OD_COLORTEXT)

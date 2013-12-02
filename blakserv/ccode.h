8f6ba15c89915fce38171d2e661aa2537c65593f
unknown <Oriumpor@yahoo.com>
2013-12-01 19:34:58 -0800

    Missed some declarations

--- blakserv/ccode.h
+++ blakserv/ccode.h
@@ -189,6 +189,10 @@ int C_GetTime(int object_id,local_var_type *local_vars,
 	      int num_normal_parms,parm_node normal_parm_array[],
 	      int num_name_parms,parm_node name_parm_array[]);
 
+int C_GetTickCount(int object_id,local_var_type *local_vars,
+	      int num_normal_parms,parm_node normal_parm_array[],
+	      int num_name_parms,parm_node name_parm_array[]);
+
 int C_Random(int object_id,local_var_type *local_vars,
 	     int num_normal_parms,parm_node normal_parm_array[],
 	     int num_name_parms,parm_node name_parm_array[]);

6f9e31bd592673c37996efac6565662d3ba797e3
Daenks <dankx0r@gmail.com>
2014-01-18 00:08:29 -0600

    automerge script

--- qbscripts/automerge.pl
+++ qbscripts/automerge.pl
@@ -1,3 +1,25 @@
 #mergechangedbranches
 #create branch from buildnumber
 #merge branches from previous script
+
+use strict;
+use warnings;
+use LWP::Simple;
+use Getopt::Long;
+
+my @changelists;
+my $build;
+
+GetOptions ("change=s" => \@changelists,
+            "build=s" => \$build);
+	
+system("git checkout -b $build master");
+	
+foreach my $changelist (@changelists)
+{
+	my $RC = system("git merge $changelist");	
+	if ($RC != 0)
+	{
+		die "Merge of branch $branch failed";
+	}
+}

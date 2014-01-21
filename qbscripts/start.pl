59d324a871182d68d08f0cf643664d5dd409ed70
Daenks <dankx0r@gmail.com>
2014-01-20 19:33:55 -0600

    fixed start.pl to start blakserv correctly and not wait for a return code.

--- qbscripts/start.pl
+++ qbscripts/start.pl
@@ -1,19 +1,22 @@
 use Net::Telnet ();
 use File::Copy;
 
-chdir "C:/Users/Administrator/Desktop/server104";
-system("C:/Users/Administrator/Desktop/server104/blakserv.exe");
+$SIG{CHLD} = 'IGNORE';
 
-print "Waiting 30 seconds for blakserv to start\n";
-sleep 30;
+print "Starting blakserv...\n";
+chdir "C:/Users/Administrator/Desktop/server104";
+
+system(1,"C:/Users/Administrator/Desktop/server104/blakserv.exe");
+
+print "Waiting 5 seconds for blakserv to start\n";
+sleep 5;
 
 print "Connecting to server 104\n";
 $t = new Net::Telnet (Timeout => 10);
-$t->port($port);
-$t->open($host);
+$t->port(9992);
+$t->open("localhost");
 print "Setting Daenks' password to \"1\"\n";
 $t->print("set account password 1 1");
 sleep(1);
 print "Sending recreateall\n";
 $t->print("send o 0 RecreateAll");
-sleep(1);

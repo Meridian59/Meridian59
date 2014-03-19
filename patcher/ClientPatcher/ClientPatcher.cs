e91829c11c84b5b0bff4d1c6fbc6c1478b85bcdf
Daenks <dankx0r@gmail.com>
2014-03-19 08:57:06 -0500

    Launcher/Patcher v0.7
    No functionality changes, started taking code from the main form and
    splitting it out into classes. These functions have yet to be removed
    from the main form.
    
    new classes:
    ClientPatcher - Responsible for doing the actual patching
    ClientProfileManager - Deals with loading/saving/changing settings

---
+++ patcher/ClientPatcher/ClientPatcher.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+using PatchListGenerator;
+using System.IO;
+using System.Diagnostics;
+
+namespace ClientPatcher
+{
+ 
+
+    class ClientPatcher
+    {
+        private string PatchInfoJason;
+
+        private List<ManagedFile> PatchFiles; //Loaded from the web server at PatchInfoURL
+        private List<ManagedFile> LocalFiles; //Loaded with files that do NOT match
+
+        private PatcherSettings CurrentProfile;
+
+        private long PatchTotalSize = 0;
+
+        private int DownloadJson()
+        {
+            WebClient wc = new WebClient();
+            try
+            {
+                PatchInfoJason = wc.DownloadString(CurrentProfile.PatchInfoURL);
+                PatchFiles = JsonConvert.DeserializeObject<List<ManagedFile>>(PatchInfoJason);
+                return 1;
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine("WebException Handler: {0}", e.ToString());
+                return 0;
+            }
+        }
+
+        private void ScanClient()
+        {
+            string fullpath;
+            Directory.CreateDirectory(CurrentProfile.ClientFolder + "\\resource\\");
+            Directory.CreateDirectory(CurrentProfile.ClientFolder + "\\download\\");
+            Directory.CreateDirectory(CurrentProfile.ClientFolder + "\\help\\");
+            Directory.CreateDirectory(CurrentProfile.ClientFolder + "\\mail\\");
+            Directory.CreateDirectory(CurrentProfile.ClientFolder + "\\ads\\");
+
+            foreach (ManagedFile PatchFile in PatchFiles)
+            {
+                fullpath = CurrentProfile.ClientFolder + PatchFile.basepath + PatchFile.filename;
+                ManagedFile LocalFile = new ManagedFile(fullpath);
+                LocalFile.ComputeHash();
+                if (PatchFile.myHash != LocalFile.myHash)
+                {
+                    LocalFiles.Add(LocalFile);
+                    LocalFile.Length = PatchFile.Length;
+                    PatchTotalSize += PatchFile.Length;
+                }
+            }
+        }
+
+        private void DownloadFiles()
+        {
+            WebClient client = new WebClient();
+
+            foreach (ManagedFile File in LocalFiles)
+            {
+                string temp = File.basepath.Replace("\\", "/");
+                try
+                {
+                    client.DownloadFile(CurrentProfile.PatchBaseURL + temp + File.filename, CurrentProfile.ClientFolder + File.basepath + File.filename);
+                }
+                catch (WebException e)
+                {
+                    Console.WriteLine("WebException Handler: {0}", e.ToString());
+                    return;
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ patcher/ClientPatcher/ClientProfileManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientPatcher
+{
+    class ClientProfileManager
+    {
+        private string SettingsPath; //Path to JSON file settings.txt
+        private List<PatcherSettings> Servers; //Loaded from settings.txt, or generated on first run and then saved.
+
+        public void LoadSettings()
+        {
+            SettingsPath = Directory.GetCurrentDirectory() + "\\settings.txt";
+            if (File.Exists(SettingsPath))
+            {
+                StreamReader file = File.OpenText(SettingsPath);
+                JsonSerializer js = new JsonSerializer();
+                //ps = JsonConvert.DeserializeObject<PatcherSettings>(file.ReadToEnd());
+                Servers = JsonConvert.DeserializeObject<List<PatcherSettings>>(file.ReadToEnd());
+                file.Close();
+            }
+            else
+            {
+                Servers = new List<PatcherSettings>();
+                Servers.Add(new PatcherSettings(103));
+                Servers.Add(new PatcherSettings(104));
+                SaveSettings();
+            }
+            PatcherSettings DefaultProfile = Servers.Find(x => x.Default == true);
+            int index = Servers.FindIndex(x => x.Default == true);
+            if (DefaultProfile != null)
+            {
+                CurrentProfile = DefaultProfile;
+            }
+        }
+
+        private void SaveSettings()
+        {
+            using (StreamWriter sw = new StreamWriter(SettingsPath))
+            {
+                sw.Write(JsonConvert.SerializeObject(Servers, Formatting.Indented));
+            }
+        }
+
+        public void AddProfile(string clientfolder, string patchbaseurl, string patchinfourl, string servername, bool isdefault)
+        {
+            PatcherSettings ps = new PatcherSettings();
+            ps.ClientFolder = clientfolder;
+            ps.PatchBaseURL = patchbaseurl;
+            ps.PatchInfoURL = patchinfourl;
+            ps.ServerName = servername;
+            ps.Default = isdefault;
+
+            Servers.Add(ps);
+            SaveSettings();
+            LoadSettings();
+        }
+
+        public void AddProfile(PatcherSettings newprofile)
+        {
+            Servers.Add(newprofile);
+            SaveSettings();
+            LoadSettings();
+        }
+
+    }
+}

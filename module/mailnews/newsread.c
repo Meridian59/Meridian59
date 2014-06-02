b6c45a90455a3e51bffc529a66d3d900b806abb9
skittles1 <tmx883@gmail.com>
2014-06-02 08:01:39 +0800

    time offset

--- module/mailnews/newsread.c
+++ module/mailnews/newsread.c
@@ -383,7 +383,7 @@ Bool DateFromSeconds(long seconds, char *str)
    struct tm *t;
    time_t local_time = seconds;
 
-   local_time += 1267405200L;    // Offset to sometime in March 2010
+   local_time += 1388534400L;    // Offset to January 2014
    t = localtime(&local_time);
 
    if (t == NULL)

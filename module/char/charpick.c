ce3ef7f60d5986fe968efb28bc5025f255a82f18
si <wiseoldsi@gmail.com>
2014-08-25 12:07:58 +0100

    Careful formatting of new sorting section

--- module/char/charpick.c
+++ module/char/charpick.c
@@ -6,19 +6,19 @@
 //
 // Meridian is a registered trademark.
 /*
-* charpick.c:  Let player select a character to use in the game.
-*/
+ * charpick.c:  Let player select a character to use in the game.
+ */
 
 #include "client.h"
 #include "char.h"
 
 // Parameters to pick char dialog box
 typedef struct {
-	Character     *characters;
-	WORD           num_characters;
-	char          *motd;
-	BYTE           num_ads;
-	Advertisement *ads;
+   Character     *characters;
+   WORD           num_characters;
+   char          *motd;
+   BYTE           num_ads;
+   Advertisement *ads;
 } PickCharStruct;
 
 ID    char_to_use;   /* ID # of character player wants to use in game */
@@ -39,248 +39,248 @@ static void CharPickLButtonDown(HWND hwnd, BOOL fDoubleClick, int x, int y, UINT
 /* character is id of character to use */
 void GoToGame(ID character)
 {
-	RequestUseCharacter(character);
-	GameSetState(GAME_INIT);  // Start up game interface
+   RequestUseCharacter(character);
+   GameSetState(GAME_INIT);  // Start up game interface
 }
 /********************************************************************/
 void AbortCharDialogs(void)
 {
-	Bool has_dialog = False;
+   Bool has_dialog = False;
 
-	if (hPickCharDialog != NULL)
-	{
-		has_dialog = True;
-		EndDialog(hPickCharDialog, IDCANCEL);
-	}
-	if (hMakeCharDialog != NULL)
-	{
-		has_dialog = True;
-		PropSheet_PressButton(hMakeCharDialog, PSBTN_CANCEL);
-		// Hide in case another dialog pops up; for some reason the property sheet lingers awhile
-		ShowWindow(hMakeCharDialog, SW_HIDE);
-	}
-	exiting = True;
+   if (hPickCharDialog != NULL)
+   {
+      has_dialog = True;
+      EndDialog(hPickCharDialog, IDCANCEL);
+   }
+   if (hMakeCharDialog != NULL)
+   {
+      has_dialog = True;
+      PropSheet_PressButton(hMakeCharDialog, PSBTN_CANCEL);
+      // Hide in case another dialog pops up; for some reason the property sheet lingers awhile
+      ShowWindow(hMakeCharDialog, SW_HIDE);
+   }
+   exiting = True;
 
-	if (!has_dialog)
-		PostMessage(cinfo->hMain, BK_MODULEUNLOAD, 0, MODULE_ID);
+   if (!has_dialog)
+      PostMessage(cinfo->hMain, BK_MODULEUNLOAD, 0, MODULE_ID);
 }
 /********************************************************************/
 /*
-* ChooseCharacter:  Ask user to choose a character from the given characters.
-*/
+ * ChooseCharacter:  Ask user to choose a character from the given characters.
+ */
 void ChooseCharacter(Character *characters, WORD num_characters, char *motd,
-					 BYTE num_ads, Advertisement *ads, BOOL quickstart)
+		     BYTE num_ads, Advertisement *ads, BOOL quickstart)
 {
-	int retval;
-	PickCharStruct s;
+   int retval;
+   PickCharStruct s;
 
-	if ((num_characters == 1) && quickstart && (characters->flags != 1))
-	{
-		char_to_use = characters->id;
-		GoToGame(char_to_use);
-		return;
-	}
+   if ((num_characters == 1) && quickstart && (characters->flags != 1))
+   {
+      char_to_use = characters->id;
+      GoToGame(char_to_use);
+      return;
+   }
 
-	GameSetState(GAME_PICKCHAR);
+   GameSetState(GAME_PICKCHAR);
 
-	s.characters     = characters;
-	s.num_characters = num_characters;
-	s.motd           = motd;
-	s.num_ads        = num_ads;
-	s.ads            = ads;
+   s.characters     = characters;
+   s.num_characters = num_characters;
+   s.motd           = motd;
+   s.num_ads        = num_ads;
+   s.ads            = ads;
 
-	retval = DialogBoxParam(hInst, MAKEINTRESOURCE(IDD_CHARPICK), cinfo->hMain,PickCharDialogProc, (LPARAM) &s);
+   retval = DialogBoxParam(hInst, MAKEINTRESOURCE(IDD_CHARPICK), cinfo->hMain,PickCharDialogProc, (LPARAM) &s);
 
-	CharactersDestroy(characters, num_characters);
+   CharactersDestroy(characters, num_characters);
 
-	switch (retval)
-	{
-	case IDCANCEL:
-		return;
+   switch (retval)
+   {
+   case IDCANCEL:
+      return;
 
-	case IDOK:
-		GoToGame(char_to_use);
-		return;
+   case IDOK:
+      GoToGame(char_to_use);
+      return;
 
-	case IDC_RESET:
-		/* Player wants to start a new character */
-		RequestCharInfo(); 
-		break;
-	}
+   case IDC_RESET:
+      /* Player wants to start a new character */
+      RequestCharInfo(); 
+      break;
+   }
 }
 /********************************************************************/
 BOOL CALLBACK PickCharDialogProc(HWND hDlg, UINT message, UINT wParam, LONG lParam)
 {
-	static HWND hList;
-	int index, i;
-	Character *c;
+   static HWND hList;
+   int index, i;
+   Character *c;
 
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		CenterWindow(hDlg, GetParent(hDlg));
-		hList = GetDlgItem(hDlg, IDC_CHARLIST);
-		info = (PickCharStruct *) lParam;
+   switch (message)
+   {
+   case WM_INITDIALOG:
+      CenterWindow(hDlg, GetParent(hDlg));
+      hList = GetDlgItem(hDlg, IDC_CHARLIST);
+      info = (PickCharStruct *) lParam;
 
-		// Sort thy account owned characters by their god given names alphabetically with new character slots *always* appearing last.
-		// No more shall we suffer having to press down to select our only character!
-		bool bSorted = false;
-		Character tmpCharacter;          
-		while (false == bSorted) 
-		{
-			bSorted = true;
-			for (i = 0; i < info->num_characters-1; i++)
-			{                
-				if ( (info->characters[i].flags == 1 && info->characters[i+1].flags != 1) 
-					||  ( info->characters[i].flags != 1 && info->characters[i+1].flags != 1 && 
-					info->characters[i].name != NULL && info->characters[i+1].name != NULL 
-					&& strcmpi(info->characters[i].name, info->characters[i+1].name) > 0 ) )
-				{        
-					// bubble all new character slots to the end of the list and non-alphabetical character positions.                        
-					tmpCharacter = info->characters[i];
-					info->characters[i] = info->characters[i+1];
-					info->characters[i+1] = tmpCharacter;
-					bSorted = false;
-				}
-			}
-		}        // end while          
+      // Sort thy account owned characters by their god given names alphabetically with new character slots *always* appearing last.
+      // No more shall we suffer having to press down to select our only character!
+      bool bSorted = false;
+      Character tmpCharacter;          
+      while (false == bSorted) 
+      {
+         bSorted = true;
+         for (i = 0; i < info->num_characters-1; i++)
+         {                
+            if ( (info->characters[i].flags == 1 && info->characters[i+1].flags != 1) 
+            ||  ( info->characters[i].flags != 1 && info->characters[i+1].flags != 1 && 
+            info->characters[i].name != NULL && info->characters[i+1].name != NULL 
+            && strcmpi(info->characters[i].name, info->characters[i+1].name) > 0 ) )
+            {        
+               // bubble all new character slots to the end of the list and non-alphabetical character positions.
+               tmpCharacter = info->characters[i];
+               info->characters[i] = info->characters[i+1];
+               info->characters[i+1] = tmpCharacter;
+               bSorted = false;
+               }
+         }
+      }        // end while    
 
-		/* Display characters in list */
-		for (i=0; i < info->num_characters; i++)
-		{
-			// For new characters, show a special string (<New Character>)
-			if (info->characters[i].flags == 1)
-				index = ListBox_AddString(hList, GetString(hInst, IDS_NEWCHARACTER));
-			else index = ListBox_AddString(hList, info->characters[i].name);
-			ListBox_SetItemData(hList, index, i);
-		}
+      /* Display characters in list */
+      for (i=0; i < info->num_characters; i++)
+      {
+ 	 // For new characters, show a special string
+  	 if (info->characters[i].flags == 1)
+	   index = ListBox_AddString(hList, GetString(hInst, IDS_NEWCHARACTER));
+	   else index = ListBox_AddString(hList, info->characters[i].name);
+	 ListBox_SetItemData(hList, index, i);
+      }
 
-		/* Select first char */
-		ListBox_SetCurSel(hList, 0);
+      /* Select first char */
+      ListBox_SetCurSel(hList, 0);
 
-		// Show message of the day
-		Edit_SetText(GetDlgItem(hDlg, IDC_MOTD), info->motd);  
+      // Show message of the day
+      Edit_SetText(GetDlgItem(hDlg, IDC_MOTD), info->motd);  
 
-		// Display advertisements
-		for (i=0; i < info->num_ads; i++)
-		{
-			char filename[MAX_PATH + FILENAME_MAX];
-			sprintf(filename, "%s\\%s", ad_directory, info->ads[i].filename);
-			Animate_Open(GetDlgItem(hDlg, animation_controls[i]), filename);
-		}
+      // Display advertisements
+      for (i=0; i < info->num_ads; i++)
+      {
+	char filename[MAX_PATH + FILENAME_MAX];
+	 sprintf(filename, "%s\\%s", ad_directory, info->ads[i].filename);
+	 Animate_Open(GetDlgItem(hDlg, animation_controls[i]), filename);
+      }
 
-		hPickCharDialog = hDlg;
-		return TRUE;
+      hPickCharDialog = hDlg;
+      return TRUE;
 
-	case WM_COMPAREITEM:
-		return ItemListCompareItem(hDlg, (const COMPAREITEMSTRUCT *) lParam);
-	case WM_MEASUREITEM:
-		ItemListMeasureItem(hDlg, (MEASUREITEMSTRUCT *) lParam);
-		return TRUE;
-	case WM_DRAWITEM:
-		return ItemListDrawItem(hDlg, (const DRAWITEMSTRUCT *) lParam);
+   case WM_COMPAREITEM:
+      return ItemListCompareItem(hDlg, (const COMPAREITEMSTRUCT *) lParam);
+   case WM_MEASUREITEM:
+      ItemListMeasureItem(hDlg, (MEASUREITEMSTRUCT *) lParam);
+      return TRUE;
+   case WM_DRAWITEM:
+      return ItemListDrawItem(hDlg, (const DRAWITEMSTRUCT *) lParam);
 
-		HANDLE_MSG(hDlg, WM_LBUTTONDOWN, CharPickLButtonDown);
+      HANDLE_MSG(hDlg, WM_LBUTTONDOWN, CharPickLButtonDown);
+	 
+      HANDLE_MSG(hDlg, WM_CTLCOLOREDIT, DialogCtlColor);
+      HANDLE_MSG(hDlg, WM_CTLCOLORLISTBOX, DialogCtlColor);
+      HANDLE_MSG(hDlg, WM_CTLCOLORSTATIC, DialogCtlColor);
+      HANDLE_MSG(hDlg, WM_CTLCOLORDLG, DialogCtlColor);
 
-		HANDLE_MSG(hDlg, WM_CTLCOLOREDIT, DialogCtlColor);
-		HANDLE_MSG(hDlg, WM_CTLCOLORLISTBOX, DialogCtlColor);
-		HANDLE_MSG(hDlg, WM_CTLCOLORSTATIC, DialogCtlColor);
-		HANDLE_MSG(hDlg, WM_CTLCOLORDLG, DialogCtlColor);
+   case WM_COMMAND:
+      switch(GET_WM_COMMAND_ID(wParam, lParam))
+      {
+      case IDC_RESET:
+	 /* Get currently chosen character's name */
+	 if ((index = ListBox_GetCurSel(hList)) == LB_ERR)
+	    return TRUE;
 
-	case WM_COMMAND:
-		switch(GET_WM_COMMAND_ID(wParam, lParam))
-		{
-		case IDC_RESET:
-			/* Get currently chosen character's name */
-			if ((index = ListBox_GetCurSel(hList)) == LB_ERR)
-				return TRUE;
+	 index = ListBox_GetItemData(hList, index);
 
-			index = ListBox_GetItemData(hList, index);
+	 c = &info->characters[index];
+	 
+	 if (!AreYouSure(hInst, hDlg, NO_BUTTON, IDS_DELETECHAR, c->name))
+	    return TRUE;
 
-			c = &info->characters[index];
+	 char_to_use = c->id;
+	 strcpy(name_to_use, c->name);
+	 EndDialog(hDlg, IDC_RESET);
+	 return TRUE;
+	 
+      case IDOK:
+	 /* Tell server that we want to use selected character */
+	 if ((index = ListBox_GetCurSel(hList)) == LB_ERR)
+	    return TRUE;
 
-			if (!AreYouSure(hInst, hDlg, NO_BUTTON, IDS_DELETECHAR, c->name))
-				return TRUE;
+	 index = ListBox_GetItemData(hList, index);
 
-			char_to_use = c->id;
-			strcpy(name_to_use, c->name);
-			EndDialog(hDlg, IDC_RESET);
-			return TRUE;
+	 c = &info->characters[index];
+	 
+	 char_to_use = c->id;
+	 strcpy(name_to_use, c->name);
 
-		case IDOK:
-			/* Tell server that we want to use selected character */
-			if ((index = ListBox_GetCurSel(hList)) == LB_ERR)
-				return TRUE;
+	 // If this character hasn't been used before, go to char creation
+	 if (c->flags == 1)
+	    EndDialog(hDlg, IDC_RESET);
+	 else EndDialog(hDlg, IDOK);
+	 return TRUE;
+	 
+      case IDCANCEL:
+	 /* If user verifies, ask to leave game */	 
+	 if (!AreYouSure(cinfo->hInst, hDlg, NO_BUTTON, IDS_LOGOFF))
+	    break;
+	 Logoff();
 
-			index = ListBox_GetItemData(hList, index);
+	 EndDialog(hDlg, IDCANCEL);
+	 return TRUE;
+      }
+      break;
 
-			c = &info->characters[index];
-
-			char_to_use = c->id;
-			strcpy(name_to_use, c->name);
-
-			// If this character hasn't been used before, go to char creation
-			if (c->flags == 1)
-				EndDialog(hDlg, IDC_RESET);
-			else EndDialog(hDlg, IDOK);
-			return TRUE;
-
-		case IDCANCEL:
-			/* If user verifies, ask to leave game */	 
-			if (!AreYouSure(cinfo->hInst, hDlg, NO_BUTTON, IDS_LOGOFF))
-				break;
-			Logoff();
-
-			EndDialog(hDlg, IDCANCEL);
-			return TRUE;
-		}
-		break;
-
-	case WM_DESTROY:
-		hPickCharDialog = NULL;
-		if (exiting)
-			PostMessage(cinfo->hMain, BK_MODULEUNLOAD, 0, MODULE_ID);
-		break;
-	}
-	return FALSE;
+   case WM_DESTROY:
+      hPickCharDialog = NULL;
+      if (exiting)
+	 PostMessage(cinfo->hMain, BK_MODULEUNLOAD, 0, MODULE_ID);
+      break;
+   }
+   return FALSE;
 }
 /********************************************************************/
 /*
-* CharactersDestroy:  Free memory for given characters array.
-*/
+ * CharactersDestroy:  Free memory for given characters array.
+ */
 void CharactersDestroy(Character *c, WORD num_characters)
 {
-	int i;
+   int i;
 
-	for (i=0; i < num_characters; i++)
-		SafeFree(c[i].name);
-	SafeFree(c);
+   for (i=0; i < num_characters; i++)
+      SafeFree(c[i].name);
+   SafeFree(c);
 }
 /****************************************************************************/
 /*
-* CharPickLButtonDown:  Mouse button pressed on dialog; see if inside an ad
-*   control and bring up browser on URL if so.
-*/ 
+ * CharPickLButtonDown:  Mouse button pressed on dialog; see if inside an ad
+ *   control and bring up browser on URL if so.
+ */ 
 void CharPickLButtonDown(HWND hwnd, BOOL fDoubleClick, int x, int y, UINT keyFlags)
 {
-	int i;
-	RECT rect;
-	POINT p;
+   int i;
+   RECT rect;
+   POINT p;
 
-	p.x = x;
-	p.y = y;
+   p.x = x;
+   p.y = y;
 
-	ClientToScreen(hwnd, &p);
-	for (i=0; i < info->num_ads; i++)
-	{
-		GetWindowRect(GetDlgItem(hPickCharDialog, animation_controls[i]), &rect);
-		if (PtInRect(&rect, p))
-		{
-			WebLaunchBrowser(info->ads[i].url);
+   ClientToScreen(hwnd, &p);
+   for (i=0; i < info->num_ads; i++)
+   {
+      GetWindowRect(GetDlgItem(hPickCharDialog, animation_controls[i]), &rect);
+      if (PtInRect(&rect, p))
+      {
+	 WebLaunchBrowser(info->ads[i].url);
 
-			// Tell server that user selected ad
-			SendAdSelected(i + 1);
-			return;
-		}
-	}
+	 // Tell server that user selected ad
+	 SendAdSelected(i + 1);
+	 return;
+      }
+   }
 }

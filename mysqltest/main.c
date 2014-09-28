3aa8b8d5bbce867af9f6afa2e4309d0a4635630e
cyberjunk <clint.banzhaf@gmx.net>
2014-09-28 02:36:06 +0200

    improve mysql test

--- mysqltest/main.c
+++ mysqltest/main.c
@@ -22,41 +22,99 @@ int _tmain(int argc, _TCHAR* argv[])
 		
 	while (TRUE)
 	{
-		/************************** TEST 1 **************************************/
-		/*						SINGLE WRITE									*/
 		/************************************************************************/
-		printf("Running Test 1: Write one of each record...\n");
-		MySQLRecordTotalMoney(1000);
-		MySQLRecordMoneyCreated(100);
-		MySQLRecordPlayerLogin("test1acc", "test1char", "127.0.0.1");
-		MySQLRecordPlayerAssessDamage("test1who", "test1attacker", 1, 2, 3, 4, "test1weapon");
+		/*							TEST 1										*/
+		/************************************************************************/
+		printf("-------------------------------------------\n");
+		printf("    Test 1: Write one of each record...    \n");
+		printf("-------------------------------------------\n");
+
+		if (!MySQLRecordTotalMoney(1000))
+			printf("T1:%i\t - TotalMoney\n", i);
+		else 
+			printf("T1:%i\t + TotalMoney\n", i);
+
+		if (!MySQLRecordMoneyCreated(100))
+			printf("T1:%i\t - MoneyCreated\n", i);
+		else
+			printf("T1:%i\t + MoneyCreated\n", i);
+
+		if (!MySQLRecordPlayerLogin("test1acc", "test1char", "127.0.0.1"))
+			printf("T1:%i\t - PlayerLogin\n", i);
+		else
+			printf("T1:%i\t + PlayerLogin\n", i);
+
+		if (!MySQLRecordPlayerAssessDamage("test1who", "test1attacker", 1, 2, 3, 4, "test1weapon"))
+			printf("T1:%i\t - PlayerAssessDamage\n", i);
+		else
+			printf("T1:%i\t + PlayerAssessDamage\n", i);
+		
 		Sleep(SLEEPWRITESHORT);
 		
-		/************************** TEST 2 **************************************/
-		/*						FILL UP QUEUE                                   */
 		/************************************************************************/
-		printf("Running Test 2: Fill up queue fast...\n");
+		/*								TEST 2                                  */
+		/************************************************************************/
+		printf("-------------------------------------------\n");
+		printf("     Test 2: Hit queue limit...            \n");
+		printf("-------------------------------------------\n");
+		
 		for(i = 0; i < 1000; i++)
 		{
-			MySQLRecordTotalMoney(2000);
-			MySQLRecordMoneyCreated(200);
-			MySQLRecordPlayerLogin("test2acc", "test2char", "172.24.5.1");
-			MySQLRecordPlayerAssessDamage("test2who", "test2attacker", 1, 2, 3, 4, "test2weapon");
+			if (!MySQLRecordTotalMoney(2000))
+				printf("T2:%i\t - TotalMoney\n", i);
+			else 
+				printf("T2:%i\t + TotalMoney\n", i);
+
+			if (!MySQLRecordMoneyCreated(200))
+				printf("T2:%i\t - MoneyCreated\n", i);
+			else
+				printf("T2:%i\t + MoneyCreated\n", i);
+
+			if (!MySQLRecordPlayerLogin("test2acc", "test2char", "172.24.5.1"))
+				printf("T2:%i\t - PlayerLogin\n", i);
+			else
+				printf("T2:%i\t + PlayerLogin\n", i);
+
+			if (!MySQLRecordPlayerAssessDamage("test2who", "test2attacker", 1, 2, 3, 4, "test2weapon"))
+				printf("T2:%i\t - PlayerAssessDamage\n", i);
+			else
+				printf("T2:%i\t + PlayerAssessDamage\n", i);
 		}
+		
 		Sleep(SLEEPWRITELONG);
 		
-		/************************** TEST 3 **************************************/
-		/*					MANY WRITES WITH WAITS								*/
 		/************************************************************************/
-		printf("Running Test 3: Lots of writes with waits...\n");
+		/*								TEST 3                                  */
+		/************************************************************************/
+		printf("-------------------------------------------\n");
+		printf("    Test 3: Writes with 100ms waits...     \n");
+		printf("-------------------------------------------\n");
+		
 		for(i = 0; i < 1000; i++)
 		{
-			MySQLRecordTotalMoney(3000);
-			MySQLRecordMoneyCreated(300);
-			MySQLRecordPlayerLogin("test3acc", "test3char", "122.1.5.1");
-			MySQLRecordPlayerAssessDamage("test3who", "test3attacker", 1, 2, 3, 4, "test3weapon");
-			Sleep(50);
+			if(!MySQLRecordTotalMoney(3000))
+				printf("T3:%i\t - TotalMoney\n", i);
+			else 
+				printf("T3:%i\t + TotalMoney\n", i);
+
+			if (!MySQLRecordMoneyCreated(300))
+				printf("T3:%i\t - MoneyCreated\n", i);
+			else
+				printf("T3:%i\t + MoneyCreated\n", i);
+
+			if (!MySQLRecordPlayerLogin("test3acc", "test3char", "122.1.5.1"))
+				printf("T3:%i\t - PlayerLogin\n", i);
+			else
+				printf("T3:%i\t + PlayerLogin\n", i);
+
+			if (!MySQLRecordPlayerAssessDamage("test3who", "test3attacker", 1, 2, 3, 4, "test3weapon"))
+				printf("T3:%i\t - PlayerAssessDamage\n", i);
+			else
+				printf("T3:%i\t + PlayerAssessDamage\n", i);
+
+			Sleep(100);
 		}
+		
 		Sleep(SLEEPWRITELONG);
 	}
 

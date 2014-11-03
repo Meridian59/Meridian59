599b289481c42e6bccb8272485ab9f31ae85b667
skittles1 <tmx883@gmail.com>
2014-11-04 05:27:53 +0800

    NPC speech library fixes

--- dlg/dialogconvert.pl
+++ dlg/dialogconvert.pl
@@ -7,22 +7,38 @@ my $outfile = 'npcdlg.txt';
 open my $info, $infile or die "Could not open $infile: $!";
 open my $fh, '>>', $outfile or die "Could not open file '$outfile' $!";
 
-while( my $line = <$info>)  
+while( my $line = <$info>)
 {
 	my @values = split('\t',$line);
-	my $command1 = "send o 0 speechlibtrigger who class $values[0]";
+	my $command1 = "send o 0 speechlibtrigger who class $values[0] trigger q $values[2]";
 	my $command2 = "send o 0 speechlibquote who class $values[0] quote q $values[3]\n";
+	my $command3 = "send o 0 randomlibquote who class $values[0]";
 	#print "0:$values[0]\n1:$values[1]\n2:$values[2]\n3:$values[3]\n----\n";
-	if (length $values[1])
+	# Check if we have a quote
+	if (length $values[3])
 	{
-		$command1 = $command1 . " mood int $values[1]";
+		# If we have a trigger, use command1 and command 2. If not, use command 3
+		if (length $values[2])
+		{
+			# Add the mood if present
+			if (length $values[1])
+			{
+				$command1 = $command1 . " mood int $values[1]";
+			}
+			print $fh "$command1\n$command2";
+			#last if $. == 200;
+		}
+		else
+		{
+			# Add the mood if present
+			if (length $values[1])
+			{
+				$command3 = $command3 . " mood int $values[1]";
+			}
+			$command3 = $command3 . " quote q $values[3]";
+			print $fh "$command3\n";
+		}
 	}
-	if (length $values[2])
-	{
-		$command1 = $command1 . " trigger q $values[2]";
-	}
-	print $fh "$command1\n$command2";
-    #last if $. == 200;
 }
 
 close $infile;

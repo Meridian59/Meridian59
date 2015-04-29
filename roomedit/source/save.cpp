1cfbcb0f2b4aff7f06925fc6bfa338f8cab55d6d
cyberjunk <clint.banzhaf@gmx.net>
2015-04-29 15:53:05 +0200

    Fix roomeditwalls write/load short/int issue

--- roomedit/source/save.cpp
+++ roomedit/source/save.cpp
@@ -396,6 +396,7 @@ void SaveRoomeditWalls(FILE *file)
    int i;
    SideDef SD1, SD2;
    WORD word;
+   WORD payload = 0;
    Vertex VStart, VEnd;
 
    // Write out walls
@@ -448,12 +449,20 @@ void SaveRoomeditWalls(FILE *file)
 	 word = SD2.sector;
       WriteBytes(file, &word, 2);
 
-      VStart = Vertexes[CurLD.start];
+		VStart = Vertexes[CurLD.start];
 		VEnd   = Vertexes[CurLD.end];
-		WriteBytes(file, &VStart.x, 4);
-		WriteBytes(file, &VStart.y, 4);
-		WriteBytes(file, &VEnd.x, 4);
-		WriteBytes(file, &VEnd.y, 4);
+
+		// coordinates are 16bit short
+		// fileformat has 32bit int
+		// we write some 0 in the second 16bit
+		WriteBytes(file, &VStart.x, 2);
+		WriteBytes(file, &payload, 2);
+		WriteBytes(file, &VStart.y, 2);
+		WriteBytes(file, &payload, 2);
+		WriteBytes(file, &VEnd.x, 2);
+		WriteBytes(file, &payload, 2);
+		WriteBytes(file, &VEnd.y, 2);
+		WriteBytes(file, &payload, 2);
 	}
 }
 /***************************************************************************/

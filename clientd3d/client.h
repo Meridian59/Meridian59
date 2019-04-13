eb21a66ede55aedb4853e006dd7f5bfb043afdb7
sicoderealm@github.com <sicoderealm@gmail.com>
2019-04-13 20:01:49 +0100

    Adding Secure Web Request

--- clientd3d/client.h
+++ clientd3d/client.h
@@ -49,6 +49,8 @@ enum {False = 0, True = 1};
 #define MAJOR_REV 7   /* Major version of client program */
 #define MINOR_REV 19  /* Minor version of client program; must be in [0, 99] */
 
+#define VERSION_NUMBER(major_rev, minor_rev) ((minor_rev + 100) * major_rev)
+
 #define MAXAMOUNT 9     /* Max # of digits in a server integer */
 #define MAXSTRINGLEN 255 /* Max length of a string loaded from string table */
 

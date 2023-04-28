69133e3aa2febabe664cfcd0a55f495f46e102f0
Adrien Laws <adrienlaws@hotmail.com>
2023-04-28 17:04:07 -0600

    feature: add swapliselem to adminfn.c

--- blakserv/adminfn.c
+++ blakserv/adminfn.c
@@ -2083,6 +2083,7 @@ void AdminShowCalls(int session_id,admin_parm_type parms[],
 		case ISLIST : strcpy(c_name, "IsList"); break;
 		case SETFIRST : strcpy(c_name, "SetFirst"); break;
 		case SETNTH : strcpy(c_name, "SetNth"); break;
+		case SWAPLISTELEM : strcpy(c_name, "SwapListElem"); break;
 		case DELLISTELEM : strcpy(c_name, "DelListElem"); break;
 		case FINDLISTELEM : strcpy(c_name, "FindListElem"); break;
 		case MOVELISTELEM : strcpy(c_name, "MoveListElem"); break;

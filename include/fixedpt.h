6c4979dd430cb6f245005336b18e154652218e0e
Meridian59 <meridian59.email@gmail.com>
2023-05-13 20:10:49 -0700

    Delete some unused fixed point code.

--- include/fixedpt.h
+++ include/fixedpt.h
@@ -15,21 +15,15 @@
 /* Definitions for 32 bit fixed-point math */
 
 typedef long fix;
-//typedef unsigned long fix;
+
 #define FIX_INT     16   /* # of bits in integer part */
 #define FIX_DECIMAL 16   /* # of bits in decimal part */
 
 #define LongToFix(x)  (((long) (x)) << FIX_DECIMAL)
 /* Round fix to nearest long */
 #define FixToLong(x)  ((((long) (x)) + (1L << (FIX_DECIMAL - 1))) >> FIX_DECIMAL)
-/* Truncate decimal */
-#define FixToLongTrunc(x)  (((long) (x)) >> FIX_DECIMAL)
 
 #define FloatToFix(x) ((fix) ((x) * (1L << FIX_DECIMAL)))
-#define FixToFloat(x) (((float) (x)) / (1L << FIX_DECIMAL))
-
-
-#define BIGFIXINT LongToFix(200L)  /* For "infinite" tan and cot values */
 
 // Ignore lack of return value
 #pragma warning(disable:4035)
@@ -42,19 +36,6 @@ __inline fix FixMul(int x, int y)
       mov eax,x              
       imul y                 
       shrd eax,edx,16        
-   }                           
-}
-
-__inline fix FixDiv(int x, int y)
-{
-   _asm                      
-   {                         
-      mov eax,x              
-      mov edx,eax            
-      sar edx,16             
-      shl eax,16             
-      idiv y                 
-      shrd edx,eax,16        
    }                           
 }
 

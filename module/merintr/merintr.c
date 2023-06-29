d752efdeee1b1da59284e44bd18ae55a56bd035c
Si <sicoderealm@gmail.com>
2023-06-29 15:58:36 +0100

    Restore players most recently chosen stat group following a system save or login (#510)
    * Restore players most recently chosen stat group following a system save.
    
    * adding eol
    
    * Adjusting to existing file formatting indentation style
    
    * Updates following PR feedback and comments
    
    * Removing debug statements
    
    * slight clean up of InterfaceRedrawModule by moving logic to RestoreActiveStatGroup
    
    * Small formatting adjustments
    
    * Adding `InventoryRedraw` back to logic that was lost in last commit
    
    * Addressing code review feedback
    
    * Moving integration point to restore active group to stats
    
    * Reinstating InterfaceRedrawModule as no changes required.
    
    * correction to comment typo

--- module/merintr/merintr.c
+++ module/merintr/merintr.c
@@ -577,6 +577,10 @@ keymap gQuickChatTable[] = {
 { 0, 0, 0},   // Must end table this way
 };
 
+extern player_info* GetPlayerInfo(void);
+extern void SetActiveStatGroup(int stat_group);
+extern int GetActiveStatGroup(void);
+
 /* local function prototypes */
 static spell       *ExtractNewSpell(char **ptr);
 static Bool         ExtractStatistic(char **ptr, Statistic *s);
@@ -1777,9 +1781,17 @@ Bool WINAPI EventConfigChanged(void)
    return True;
 }
 
-extern player_info *GetPlayerInfo(void);
-
 player_info *GetPlayer(void)
 {
    return GetPlayerInfo();
+}
+
+void SetStatGroup(int stat_group)
+{
+   SetActiveStatGroup(stat_group);
+}
+
+int GetStatGroup(void)
+{
+   return GetActiveStatGroup();
 }

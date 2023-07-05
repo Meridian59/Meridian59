e09b83482634d77142586a34888dc754d3bc41d5
Si <sicoderealm@gmail.com>
2023-07-05 16:14:26 +0100

    For each object rendered we increase the z-depth to prevent z-fighting (#511)
    * For each object rendered we increase the z-depth to prevent z-fighting.
    
    * adjust to using zbias_default as starting point
    
    * Correction to line spacing and formatting
    
    * Adjusting z fighting depths to take into consideration the case of player upper body items
    
    * Correction to increment logic and starting depth constant for regular items
    
    * Simplifying logic from feedback

--- clientd3d/d3drender.c
+++ clientd3d/d3drender.c
@@ -6841,6 +6841,9 @@ void D3DRenderObjectsDraw(d3d_render_pool_new *pPool, room_type *room,
 
 	anglePitch = PlayerGetHeightOffset();
 
+	// For each object rendered we increase the z-depth to prevent z-fighting.
+	int z_depth_inc = 0;
+
 	// base objects
 	for (curObject = 0; curObject < nitems; curObject++)
 	{
@@ -6979,7 +6982,14 @@ void D3DRenderObjectsDraw(d3d_render_pool_new *pPool, room_type *room,
 		pChunk->numPrimitives = pChunk->numVertices - 2;
 		pChunk->xLat0 = xLat0;
 		pChunk->xLat1 = xLat1;
+
 		pChunk->zBias = ZBIAS_BASE;
+		// Nodes with a bound height adjust are part of other players' upper bodies.
+		if (pRNode->boundingHeightAdjust == 0)
+		{
+			// Typical items such as reagents, keys, etc.
+			pChunk->zBias = ZBIAS_DEFAULT + (z_depth_inc++);
+		}
 
 		lastDistance = 0;
 

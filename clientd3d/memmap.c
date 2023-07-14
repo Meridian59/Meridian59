fa6f8c45f255899c30b5680ce0482606fdad4355
sicoderealm <sicoderealm@gmail.com>
2023-07-14 16:03:04 +0100

    Removing MappedFileOpenCopy(char *filename, file_node *f)

--- clientd3d/memmap.c
+++ clientd3d/memmap.c
@@ -47,40 +47,6 @@ Bool CliMappedFileOpenRead(char *filename, file_node *f)
 }
 /***************************************************************************/
 /*
- * MappedFileOpenCopy:  Open filename as a memory-mapped file for write-on-copy, 
- *   and map a view of the entire file.  Returns True on success, and fills in f.
- */
-Bool MappedFileOpenCopy(char *filename, file_node *f)
-{
-   f->fh = CreateFile(filename,GENERIC_READ | GENERIC_WRITE,0,NULL,
-		      OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN,NULL);
-   if (f->fh == INVALID_HANDLE_VALUE)
-   {
-      return False;
-   }
-
-   f->length = GetFileSize(f->fh, NULL);
-
-   f->mapfh = CreateFileMapping(f->fh,NULL,PAGE_WRITECOPY,0,f->length,NULL);
-   if (f->mapfh == NULL)
-   {
-      CloseHandle(f->fh);
-      return False;
-   }
-
-   f->mem = (char *) MapViewOfFile(f->mapfh,FILE_MAP_COPY,0,0,0);
-   if (f->mem == NULL)
-   {
-      CloseHandle(f->mapfh);
-      CloseHandle(f->fh);
-      return False;
-   }
-
-   f->ptr = f->mem;
-   return True;
-}
-/***************************************************************************/
-/*
  * MappedFileRead:  Copy num bytes from the memory-mapped file to buf.
  *   Increments the file pointer.  
  *   Returns num on success, or -1 on failure.

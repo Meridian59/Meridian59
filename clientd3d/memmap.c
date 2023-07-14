c629813273e74c8901508cb85012d6d39a38e9ba
sicoderealm <sicoderealm@gmail.com>
2023-07-14 14:14:47 +0100

    Allow simultaneous read access to game resources

--- clientd3d/memmap.c
+++ clientd3d/memmap.c
@@ -18,7 +18,7 @@
  */
 Bool CliMappedFileOpenRead(char *filename, file_node *f)
 {
-   f->fh = CreateFile(filename,GENERIC_READ,0,NULL,
+   f->fh = CreateFile(filename,GENERIC_READ,FILE_SHARE_READ,NULL,
 		      OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN,NULL);
    if (f->fh == INVALID_HANDLE_VALUE)
    {

dbe0d5783cdd00e57564ac65ef431b2a7041b4f1
CardiovascularMike <132171402+CardiovascularMike@users.noreply.github.com>
2023-08-18 14:02:32 -0400

    Change message when you try to attack yourself (#547)
    * add msg to user when they try to swing at themself
    
    * update resoucr.h to define new msg

--- clientd3d/gameuser.c
+++ clientd3d/gameuser.c
@@ -96,7 +96,10 @@ void UserAttackClosest(int action)
 		if (FindVisibleObjectById(idTarget))
 			RequestAttack(ATTACK_NORMAL, idTarget);
 		else
-			GameMessage(GetString(hInst, IDS_TARGETNOTVISIBLEFORATTACK));
+         if (idTarget == player.id)
+            GameMessage(GetString(hInst, IDS_CANTATTACKSELF));
+         else
+			   GameMessage(GetString(hInst, IDS_TARGETNOTVISIBLEFORATTACK));
 		return;
 	}
 	

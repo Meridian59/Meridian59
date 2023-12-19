002a2d3999965ba43a283b89b38af1083e039e51
David Klawitter <davidklaw@gmail.com>
2023-12-19 14:14:23 -0500

    Revert "Applies texture mode in both U and V directions"
    This reverts commit eb3bbe15b2683286f1b5cec4195da95977e9aa65.

--- clientd3d/d3drender.c
+++ clientd3d/d3drender.c
@@ -9231,15 +9231,9 @@ Bool D3DMaterialWorldDynamicChunk(d3d_render_chunk_new *pChunk)
 	}
 
 	if (pChunk->flags & D3DRENDER_CLAMP)
-	{
-		IDirect3DDevice9_SetSamplerState(gpD3DDevice, 0, D3DSAMP_ADDRESSU, D3DTADDRESS_CLAMP);
 		IDirect3DDevice9_SetSamplerState(gpD3DDevice, 0, D3DSAMP_ADDRESSV, D3DTADDRESS_CLAMP);
-	}
 	else
-	{
-		IDirect3DDevice9_SetSamplerState(gpD3DDevice, 0, D3DSAMP_ADDRESSU, D3DTADDRESS_WRAP);
 		IDirect3DDevice9_SetSamplerState(gpD3DDevice, 0, D3DSAMP_ADDRESSV, D3DTADDRESS_WRAP);
-	}
 
 	if (gD3DDriverProfile.bFogEnable)
 	{
@@ -9295,15 +9289,9 @@ Bool D3DMaterialWorldStaticChunk(d3d_render_chunk_new *pChunk)
 	}
 
 	if (pChunk->flags & D3DRENDER_CLAMP)
-	{
 		IDirect3DDevice9_SetSamplerState(gpD3DDevice, 0, D3DSAMP_ADDRESSU, D3DTADDRESS_CLAMP);
-		IDirect3DDevice9_SetSamplerState(gpD3DDevice, 0, D3DSAMP_ADDRESSV, D3DTADDRESS_CLAMP);
-	}
 	else
-	{
 		IDirect3DDevice9_SetSamplerState(gpD3DDevice, 0, D3DSAMP_ADDRESSU, D3DTADDRESS_WRAP);
-		IDirect3DDevice9_SetSamplerState(gpD3DDevice, 0, D3DSAMP_ADDRESSV, D3DTADDRESS_WRAP);
-	}
 
 	if (gD3DDriverProfile.bFogEnable)
 	{

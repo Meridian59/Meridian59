6b1c4907eec394cccb3c74d06a0c5d2fce4f7237
David Klawitter <davidklaw@gmail.com>
2024-01-03 14:48:10 -0500

    Adds documentation, pathing assurances, error handling

--- contrib/powershell/Monitor-Room-Resources.ps1
+++ contrib/powershell/Monitor-Room-Resources.ps1
@@ -1,6 +1,30 @@
-# Set the folder paths
-$sourceFolder = "../../resource/rooms" # Change this to your source folder path
-$destinationFolder = "../../run/localclient/resource" # Change this to your destination folder path
+<#
+    .DESCRIPTION
+    Monitors `/resource/rooms` for new or updated `.roo` files, and when found, copies them to `run/localclient/resource`.
+
+    .EXAMPLE
+    From the project root:
+    ./contrib/powershell/Monitor-Room-Resources.ps1
+
+    .NOTES
+    Saves time and clicks when making and testing frequent room file changes.
+#>
+
+# Get the absolute path of the directory where the script is located
+$scriptDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+# Construct the absolute paths for source and destination folders
+$sourceFolder = Join-Path -Path $scriptDirectory -ChildPath "..\..\resource\rooms"
+$destinationFolder = Join-Path -Path $scriptDirectory -ChildPath "..\..\run\localclient\resource"
+
+# Normalize the paths (resolves any "..")
+$sourceFolder = [System.IO.Path]::GetFullPath($sourceFolder)
+$destinationFolder = [System.IO.Path]::GetFullPath($destinationFolder)
+
+if (-not (Test-Path -Path $sourceFolder)) {
+  Write-Host "Directory $sourceFolder does not exist. Exiting script."
+  exit
+}
 
 # Create a FileSystemWatcher object to watch the source folder
 $watcher = New-Object System.IO.FileSystemWatcher
@@ -9,7 +33,7 @@ $watcher.IncludeSubdirectories = $false
 $watcher.EnableRaisingEvents = $true
 
 # Define the filter and types of changes to watch for
-$watcher.Filter = "*.roo" # Monitor all files, change this to something like "*.txt" to monitor only text files
+$watcher.Filter = "*.roo" # Monitor room files, change this to something like "*.txt" to monitor only text files
 $watcher.NotifyFilter = [System.IO.NotifyFilters]'FileName, LastWrite'
 
 # Define the action to take when a file is changed, created, or deleted

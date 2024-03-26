26c410974b0fa799274b5a3e744270684cf24bcb
Meridian59 <meridian59.email@gmail.com>
2024-03-25 22:07:01 -0700

    Test breaking the autobuild.

--- module/mailnews/mailread.c
+++ module/mailnews/mailread.c
@@ -1,4 +1,4 @@
-// Meridian 59, Copyright 1994-2012 Andrew Kirmse and Chris Kirmse.
+BREAK!!// Meridian 59, Copyright 1994-2012 Andrew Kirmse and Chris Kirmse.
 // All rights reserved.
 //
 // This software is distributed under a license that is described in

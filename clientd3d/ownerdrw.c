4827be259cd928569f0af83962752018e90ed362
Adrien Laws <adrienlaws@hotmail.com>
2024-04-10 20:12:51 -0600

    chore: cleanup ownerdrw.c comments and spacing

--- clientd3d/ownerdrw.c
+++ clientd3d/ownerdrw.c
@@ -342,27 +342,17 @@ void DrawOwnerListItem(const DRAWITEMSTRUCT *lpdis, Bool selected, Bool combo)
    SetBkMode(lpdis->hDC, OPAQUE);
    obj = (object_node*)lpdis->itemData;
 
-   // Determine the background color based on the selected state and the special type value - BACKGROUND, do not need flags or enum special type
    hColorBg = GetBrush(GetItemListColor(lpdis->hwndItem, (selected? SEL_BGD : UNSEL_BGD), text_color_value));
 
-   // If OD_ONLYSEL flag is set, adjust the background color - BACKGROUND, does not need flags or enum special type
    if ((style & OD_ONLYSEL) && (style & (OD_DRAWOBJ | OD_DRAWICON)))
       hColorBg = GetBrush(GetItemListColor(lpdis->hwndItem, UNSEL_BGD, text_color_value));
 
-   // Fill the item rectangle with the background color
    FillRect(lpdis->hDC, &lpdis->rcItem, hColorBg);
-   // Set text mode of the device context to transparent
    SetBkMode(lpdis->hDC, TRANSPARENT);
 
-   // Now we need to do the 2 foreground text options
-   // First get the flags if we are checking for an item not the player object like at the character select screen
-
-   // int flags = 0; commenting out OF_SPECIAL flag check
-   // Color special item text if the appropriate flags are set. Do this by getting the flags and the special_type
    if (style & (OD_DRAWOBJ | OD_DRAWICON) && obj != NULL)
    {
-      // flags = obj->flags; commenting out OF_SPECIAL flag check
-      text_color_value = (item_text_color)obj->text_color_type; // Retrieve the special type value from the object
+      text_color_value = (item_text_color)obj->text_color_type;
    }
    crColorText = GetColor(GetItemListColor(lpdis->hwndItem, (selected? SEL_FGD : UNSEL_FGD), text_color_value));
 

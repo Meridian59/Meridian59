348c54cb900309c6d20079b7c10bc28e7f2b8e60
Adrien Laws <adrienlaws@hotmail.com>
2024-06-03 21:45:29 -0600

    fix: add UpdateKeyTables function for use when loading and saving hotkey aliases.  Resolves issue of hotkey commands requiring a client restart to refresh properly.

--- module/merintr/alias.c
+++ module/merintr/alias.c
@@ -87,6 +87,21 @@ extern player_info *GetPlayer(void);
 
 /****************************************************************************/
 /*
+ * UpdateKeyTables:  Update either the classic or custom keys interfaces table
+ * depending on the client configuration, then also update the inventory key
+ * table so that the hotkey aliases work when the user is in that child window
+ */
+static void UpdateKeyTables(int key, WORD command, char *text)
+{
+   if (gbClassicKeys) 
+      AliasSetKey(interface_key_table, key, KEY_NONE, command, text);
+   else
+      AliasSetKey(gCustomKeys, key, KEY_NONE, command, text);
+   AliasSetKey(inventory_key_table, key, KEY_NONE, command, text);
+}
+
+/****************************************************************************/
+/*
  * AliasInit:  Read aliases from INI file and set them up.
  */
 void AliasInit(void)
@@ -147,12 +162,7 @@ void AliasInit(void)
 	 aliases[i].cr = True;
       }
 
-	  if (gbClassicKeys)
-		AliasSetKey(interface_key_table, aliases[i].key, KEY_NONE, command, aliases[i].text);
-	  else
-		AliasSetKey(gCustomKeys, aliases[i].key, KEY_NONE, command, aliases[i].text);
-
-      AliasSetKey(inventory_key_table, aliases[i].key, KEY_NONE, command, aliases[i].text);
+   UpdateKeyTables(aliases[i].key, command, aliases[i].text);
    }
 }
 
@@ -229,6 +239,7 @@ void AliasSave(void)
    char	destName[128];
    char	*srcName;
    player_info	*playerInfo;
+   WORD command;
 
    destName[0] = '\0';
 
@@ -252,11 +263,18 @@ void AliasSave(void)
       sprintf(temp, "F%d", i + 1);
 
       if (aliases[i].cr)
-	 strcpy(text, aliases[i].text);
+      {
+	      strcpy(text, aliases[i].text);
+         command = A_TEXTCOMMAND;
+      }
       else
-	 sprintf(text, "%s~", aliases[i].text);
-
+      {
+         sprintf(text, "%s~", aliases[i].text);
+         command = A_TEXTINSERT;
+      }
       WritePrivateProfileString(fullSection, temp, text, cinfo->ini_file);
+
+   UpdateKeyTables(aliases[i].key, command, aliases[i].text);
    }
 
    strcpy(fullSection, command_section);

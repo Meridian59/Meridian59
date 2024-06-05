591f7fef134f724c91b1cb42269eb2ae44ae8eb4
sicoderealm <sicoderealm@gmail.com>
2024-06-05 23:20:21 +0100

    Improve daily clean up job (#761)

--- aws/src/daily_cleanup/app.py
+++ aws/src/daily_cleanup/app.py
@@ -15,8 +15,8 @@ s3_client = boto3.client('s3')
 process_bucket_name = util_get_output_value("NewM59AccountsToProcessBucketName")
 audit_bucket_name = util_get_output_value("M59AccountsAuditBucketName")
 
-def move_to_audit(accounts_to_delete):
-    for account in accounts_to_delete:
+def move_to_audit(accounts_to_move):
+    for account in accounts_to_move:
         account_file = f"{account['email']}_{account['server']}.json"
         copy_source = {
             'Bucket': process_bucket_name,
@@ -50,9 +50,13 @@ def lambda_handler(event, context):
     aws_region = os.environ["region_name"]
     domain_name = os.environ["domain_name"] or meridian_domain_name
     
+    logger.info(f"Attempting to open S3 {bucket_name}:{master_database_key}")
+    db_obj = s3_client.get_object(Bucket=bucket_name, Key=master_database_key)
+    db_content = db_obj['Body'].read().decode('utf-8')
+    current_database_size = len(db_content.splitlines())
+    
     accounts_to_add = []
-    accounts_to_delete = []
-    database_size = 0
+    new_database_size = current_database_size
     
     try:
         paginator = s3_client.get_paginator('list_objects_v2')
@@ -60,13 +64,9 @@ def lambda_handler(event, context):
         
         for page in pages:
             for obj in page['Contents']:
-                #logger.info(obj)
             
                 file_obj = s3_client.get_object(Bucket=process_bucket_name, Key=obj['Key'])
                 file_content = file_obj['Body'].read().decode('utf-8')
-                
-                #logger.info(file_content)
-                #logger.info("------")
                 
                 account_obj = json.loads(file_content)
                 if "timestamp" in account_obj and "email" in account_obj:
@@ -79,37 +79,21 @@ def lambda_handler(event, context):
     except Exception as e:
         logger.error(f"Exception during clean: {e}")
         
-    if len(accounts_to_add) > 0 or len(accounts_to_delete) > 0 :
-        try:
-            logger.info(f"Attempting to open S3 {bucket_name}:{master_database_key}")
-            file_obj = s3_client.get_object(Bucket=bucket_name, Key=master_database_key)
-            file_content = file_obj['Body'].read().decode('utf-8')
-                
-            # update master database
-            if len(accounts_to_add) > 0:  
-                
-                for account in accounts_to_add:
-                    file_content += f"\"{account["username"]}\",\"{account["email"]}\",\"{account["server"]}\",\"{account["account_number"]}\"\n"
-                
-                try:
-                    s3_client.put_object(Bucket=bucket_name, Key=master_database_key, Body=file_content)
-                    database_size = len(file_content.splitlines())
-                    logger.info(f"Master database written to {bucket_name}/{object_key_read}")
-                except Exception as e:
-                    logger.error(f"Error writing content to S3: {e}")
-                    raise Exception("Error writing content to S3")
+    if len(accounts_to_add) > 0:
 
-            # Move to audit bucket
-            move_to_audit(accounts_to_add)
-            
-        except Exception as e:
-            logger.error(f"Failed to load update accounts and main database {e}")
-    else:
-        logger.info(f"Attempting to open S3 {bucket_name}:{master_database_key}")
-        file_obj = s3_client.get_object(Bucket=bucket_name, Key=master_database_key)
-        file_content = file_obj['Body'].read().decode('utf-8')
-        database_size = len(file_content.splitlines())
+        # update master database
+        for account in accounts_to_add:
+            db_content += f"\"{account["username"]}\",\"{account["email"]}\",\"{account["server"]}\",\"{account["account_number"]}\"\n"
         
+        try:
+            s3_client.put_object(Bucket=bucket_name, Key=master_database_key, Body=db_content)
+            new_database_size = len(db_content.splitlines())
+            logger.info(f"Master database written to {bucket_name}/{object_key_read}")
+        except Exception as e:
+            logger.error(f"Error writing content to S3: {e}")
+
+        # Move to audit bucket
+        move_to_audit(accounts_to_add)
 
     subject = "[Meridian 59] Clean up job ran!"
     send_to = send_from_email_address
@@ -120,8 +104,7 @@ Dear Maintainer,
 Cleaning job ran!
 
  - {len(accounts_to_add)} accounts merged to main database.
- - {len(accounts_to_delete)} stale accounts purged.
- - Master database now contains {database_size:,} accounts.
+ - Master database now contains {new_database_size:,} accounts (previously: {current_database_size:,}).
  - Took {round(time.time() - start, 2)} to complete!
 
 Have fun in the game!

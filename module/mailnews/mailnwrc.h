24180bb5a8169bf2b744077ccb6f0e78ea194ebd
David Klawitter <davidklaw@gmail.com>
2024-07-09 09:54:58 -0400

    Fixes for bitmap placement and loading (#798)
    - Moves bitmaps from client to mailnews
    - Moves bitmap loading to dialog initialization. They currently repeatedly load in a callback.

--- module/mailnews/mailnwrc.h
+++ module/mailnews/mailnwrc.h
@@ -59,6 +59,8 @@
 #define IDS_SUBJECT_GERMAN              55
 #define IDI_LETTER                      101
 #define IDB_MAILBOX                     102
+#define IDB_UPARROW                     103
+#define IDB_DOWNARROW                   104
 #define IDD_MAILSEND                    111
 #define IDD_NEWSREAD                    144
 #define IDD_NEWSPOST                    145
@@ -86,7 +88,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101

1b98025765100d51e9f5ccf9308ce93b4d6c7123
si <sicoderealm@gmail.com>
2024-11-07 19:14:56 +0000

    no const

--- clientd3d/d3drender_fx.h
+++ clientd3d/d3drender_fx.h
@@ -40,7 +40,7 @@ struct FxRenderSystemStructure {
     LPDIRECT3DTEXTURE9 backBufferTexFull;
     int fullTextureSize;
     int smallTextureSize;
-    const D3DMATRIX& transformMatrix;
+    D3DMATRIX& transformMatrix;
     unsigned int frame;
     int screenWidth;
     int	screenHeight;
@@ -55,7 +55,7 @@ struct FxRenderSystemStructure {
         LPDIRECT3DTEXTURE9 backBufferTexFullParam,
         int fullTextureSizeParam,
         int smallTextureSizeParam,
-        const D3DMATRIX& transformMatrixParam,
+        D3DMATRIX& transformMatrixParam,
         unsigned int frameParam,
         int screenWidthParam,
         int screenHeightParam)

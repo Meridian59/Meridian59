cff59d9c9b24f928c1d187df1c7f3e9a8b18b282
sicoderealm <sicoderealm@gmail.com>
2024-11-23 05:11:29 +0000

    Increment draw objects per leaf in draw bsp to allow more objects to be visible (#1015)
    When using the new renderer changes Mayhem noticed that objects are
    disappearing from view.
    Checking the busier rooms you will see many `too many objects per leaf:
    throwing some away` debug messages.
    We increase the number of leaf nodes to allow for even more objects to
    be visible in game.
    We've been optimizing the graphics and rendering for the last few years
    but this value hasn't changed for at least 12 years.
    
    With this change the error debug messages no longer appear and
    everything is visible.

--- clientd3d/drawbsp.c
+++ clientd3d/drawbsp.c
@@ -23,7 +23,9 @@
 
 #include "client.h"
 
-#define MAX_OBJS_PER_LEAF 100
+#include <algorithm>
+
+#define MAX_OBJS_PER_LEAF 250
 
 // Decimal places for computing fraction along wall
 #define LOG_FRACTION 12
@@ -2248,7 +2250,7 @@ static void WalkWall(WallData *wall, long side)
 static void WalkObjects(ObjectData *objects)
 {
    ObjectData *object;
-   long num,i,j;
+   long num,i;
    ObjectData *sort[MAX_OBJS_PER_LEAF];
    long x,y,a,b;
    long left,right;
@@ -2268,15 +2270,10 @@ static void WalkObjects(ObjectData *objects)
       }
    }
 
-   /* simple bubble sort, closest first */
-   for(i=0; i<num-1; i++)
-      for(j=i+1; j<num; j++)
-	 if (sort[i]->draw.distance > sort[j]->draw.distance)
-	 {
-	    ObjectData *tmp = sort[i];
-	    sort[i] = sort[j];
-	    sort[j] = tmp;
-	 }
+   // sort, closest first
+   std::sort(sort, sort + num, [](ObjectData* a, ObjectData* b) {
+      return a->draw.distance < b->draw.distance;
+   });
 
    for(i=0; i<num; i++)
    {

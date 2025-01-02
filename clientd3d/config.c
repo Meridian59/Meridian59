e6b7f18dd79e448f54fc82424828b4277102bd3e
si <sicoderealm@gmail.com>
2025-01-02 21:48:31 +0000

    comment

--- clientd3d/config.c
+++ clientd3d/config.c
@@ -303,7 +303,9 @@ void ConfigLoad(void)
 	   "gpuefficiencyonetimeflip", False, config_ini_file);
    if (!one_time_gpu_efficiency_enablement)
    {
-      // Perform one-time flip to enable GPU efficiency mode.
+      // Perform one-time flip to enable GPU efficiency mode regardless of current preference.
+	  // This is to ensure players start from a clean slate with maximum performance.
+	  // After this one-time flip, future player's preference will be respected.
       config.gpuEfficiency = true;
       WriteConfigInt(config_section, INIGpuEfficiencyOneTimeFlip, config.gpuEfficiency, config_ini_file);
       WritePrivateProfileString(config_section, INIGpuEfficiency, "true", config_ini_file);

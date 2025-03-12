f766eeed1b8ab7a1dacd489d95051f421a7febb4
Adrien Laws <adrienlaws@hotmail.com>
2025-03-12 10:22:09 -0600

    chore: remove unused c extension files, add fastapi configuration file config.py, adjust main.py

--- blakserv/fastapi/main.py
+++ blakserv/fastapi/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from api import router
+import config
 
 app = FastAPI(title="Meridian 59 Admin API")
 
@@ -15,11 +16,11 @@ app.add_middleware(
 )
 
 # Mount static files
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+#app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 # Include API routes
 app.include_router(router, prefix="/api/v1")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)  # Run only on localhost
+    uvicorn.run(app, host=config.API_HOST, port=config.API_PORT, reload=True)

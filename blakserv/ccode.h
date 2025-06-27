a64d95173f526ad7603db16116c804b45fd1d948
Adrien Laws <laws.adrien@gmail.com>
2025-06-27 10:59:46 -0600

    fix: add C_SendWebhook prototype

--- blakserv/ccode.h
+++ blakserv/ccode.h
@@ -245,6 +245,10 @@ blak_int C_MinigameStringToNumber(int object_id,local_var_type *local_vars,
 		  int num_normal_parms,parm_node normal_parm_array[],
 		  int num_name_parms,parm_node name_parm_array[]);
 
+blak_int C_SendWebhook(int object_id, local_var_type *local_vars,
+    int num_normal_parms, parm_node normal_parm_array[],
+    int num_name_parms, parm_node name_parm_array[]);
+
 
 void FuzzyCollapseString(char* pTarget, const char* pSource, int len);
 bool FuzzyBufferEqual(const char *s1,int len1,const char *s2,int len2);

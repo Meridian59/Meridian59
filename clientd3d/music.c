da8280184c56afe53ac97dec503bd3f846e02112
Adrien Laws <laws.adrien@gmail.com>
2025-12-23 20:58:39 -0700

    docs: restore deleted copyright message in music.

--- clientd3d/music.c
+++ clientd3d/music.c
@@ -1,4 +1,10 @@
-// Meridian 59
+// Meridian 59, Copyright 1994-2012 Andrew Kirmse and Chris Kirmse.
+// All rights reserved.
+//
+// This software is distributed under a license that is described in
+// the LICENSE file that accompanies it.
+//
+// Meridian is a registered trademark.
 /*
  * music.c:  Music playback via OpenAL Soft (OGG/WAV).
  *

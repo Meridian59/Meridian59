21c841ab26ceffe513122322dd015be330a7cc16
CyberBill <awesome.programmer@gmail.com>
2026-01-12 18:39:21 -0800

    Updated constant value after more play testing.

--- clientd3d/map.c
+++ clientd3d/map.c
@@ -78,7 +78,7 @@ static BOOL fMapCacheValid = FALSE;
 static struct map_wall_cache_t *pMapWalls = NULL;
 static float mapCacheScale = 0.0;
 static int mapNumCacheWalls = 0;
-static constexpr int MAX_ANNOTATION_TEXT_ZOOM = 60; // Unit-less number which helps define when we stop displaying annotation text
+static constexpr float MAX_ANNOTATION_TEXT_ZOOM = 5; // Unit-less number which helps define when we stop displaying annotation text
 
 /* local function prototypes */
 static void MapDrawMiniMapWalls(HDC hdc, int x, int y, room_type *room);

9e43409c8f7e4d60d836887f628e03682288b060
Neil Laguardia <32079356+neiltaco@users.noreply.github.com>
2026-05-23 23:28:59 -0700

    Added max step threshold and split up lastEndFrame.
    - Added MAX_STEP_THRESHOLD to trigger a 1-second discontinuity step if delta time gets absurdly long.
    - Split lastEndFrame into uncapped and capped versions.

--- clientd3d/clock.c
+++ clientd3d/clock.c
@@ -17,6 +17,7 @@ using namespace std::chrono;
 // Variables //
 ///////////////
 static time_point<steady_clock> lastEndFrame;
+static time_point<steady_clock> lastCappedEndFrame;
 
 // Elapsed time between frames (in seconds).
 static float deltaTime_s;
@@ -42,7 +43,6 @@ float GetDeltaTime()
 }
 
 // Returns delta time in milliseconds.
-// Used in legacy systems that still use milliseconds instead of seconds.
 int GetDeltaTime_Ms()
 {
 	return deltaTime_ms;
@@ -71,31 +71,76 @@ void InitializeTime()
 	cappedMsAccumulator = 0.0f;
 	
 	lastEndFrame = steady_clock::now();
+	lastCappedEndFrame = steady_clock::now();
 }
 
 void UpdateTime()
 {
-	// Get current time first.
 	auto currentTime = steady_clock::now();
-	// Calculate delta time (in seconds) since last frame.
+
+	// This defines a ceiling threshold (250ms) to detect a severe time discontinuity
+	// in case delta time gets absurdly long (e.g. when the window halts during a resize).
+	static constexpr duration<float> MAX_STEP_THRESHOLD = duration<float>(0.25f);
+
+	// If the threshold was reached, then a 1-second discontinuity step is reported
+	// for both uncapped and capped delta time to help clear the 'time debt'.
+	bool hasHitched = ((currentTime - lastEndFrame) > duration_cast<steady_clock::duration>(MAX_STEP_THRESHOLD));
+	
+	/////////////////////////
+	// Uncapped Delta Time //
+	/////////////////////////
 	auto frameTime = currentTime - lastEndFrame;
-	// Always slide the time window completely forward to prevent catch-up loops.
 	lastEndFrame = currentTime;
-	
-	// First, update the uncapped delta time in seconds and milliseconds.
-	deltaTime_s = duration<float>(frameTime).count();
-	// msAccumulator keeps track of fractional time so it isn't lost between frames.
-	float totalMs = (duration<float, std::milli>(frameTime).count()) + msAccumulator;
-	deltaTime_ms = static_cast<int>(totalMs);
-	msAccumulator = totalMs - static_cast<float>(deltaTime_ms);
-	
-	// Then calculate the capped delta time (at roughly 10FPS), also in seconds and milliseconds.
-	// This prevents movement and graphical effects from "spiking" if the window halts (e.g. during a resize).
+
+	if (hasHitched)
+	{
+		// Force the uncapped time to report just 1,000ms (1 sec) for this one frame.
+		// This prevents a bug where a large delta time passed into the 'msSleep' calculation
+		// for the FPS throttler created a speed burst in player movement.
+		deltaTime_s = 1.0f; 
+		deltaTime_ms = 1000;
+		msAccumulator = 0.0f;
+	}
+	else
+	{
+		// Update the uncapped delta time, both in seconds and milliseconds.
+		deltaTime_s = duration<float>(frameTime).count();
+
+		// msAccumulator keeps track of fractional time so it isn't lost between frames.
+		float totalMs = (duration<float, std::milli>(frameTime).count()) + msAccumulator;
+		deltaTime_ms = static_cast<int>(totalMs);
+		msAccumulator = totalMs - static_cast<float>(deltaTime_ms);
+	}
+	///////////////////////
+	// Capped Delta Time //
+	///////////////////////
+	auto cappedFrameTime = currentTime - lastCappedEndFrame;
+
+	// The limit for capped delta time is roughly 10FPS, or 100ms.
+	// This prevents movement and graphical effects from "spiking" if the framerate drops.
 	static constexpr duration<float> DELTA_TIME_CAP = duration<float>(0.1f);
-	auto cappedFrameTime = std::min(frameTime, duration_cast<steady_clock::duration>(DELTA_TIME_CAP));
 	
-	cappedDeltaTime_s = duration<float>(cappedFrameTime).count();
-	float totalCappedMs = (duration<float, std::milli>(cappedFrameTime).count()) + cappedMsAccumulator;
-	cappedDeltaTime_ms = static_cast<int>(totalCappedMs);
-	cappedMsAccumulator = totalCappedMs - static_cast<float>(cappedDeltaTime_ms);
+	if (hasHitched)
+	{
+		// Force the capped delta time to match the 1-sec discontinuity step for one frame.
+		// This prevents moving objects from 'sliding fast' to their destination by
+		// triggering a coordinate snap within MoveSingle().
+		cappedDeltaTime_s = 1.0f;
+		cappedDeltaTime_ms = 1000;
+		cappedMsAccumulator = 0.0f;
+	}
+	else
+	{
+		// Clamp the frame time if it reaches the delta time cap.
+		cappedFrameTime = std::min(cappedFrameTime, duration_cast<steady_clock::duration>(DELTA_TIME_CAP));
+
+		// Then update the capped delta time, also in seconds and milliseconds.
+		cappedDeltaTime_s = duration<float>(cappedFrameTime).count();
+
+		float totalCappedMs = (duration<float, std::milli>(cappedFrameTime).count()) + cappedMsAccumulator;
+		cappedDeltaTime_ms = static_cast<int>(totalCappedMs);
+		cappedMsAccumulator = totalCappedMs - static_cast<float>(cappedDeltaTime_ms);
+	}
+	// Always slide the capped anchor to 'currentTime' to clear historical lag debt.
+	lastCappedEndFrame = currentTime;
 }
